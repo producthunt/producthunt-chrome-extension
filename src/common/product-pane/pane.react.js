fb40cd193c8164991b07e8d7d4546ddad7df3ea9
Veselin Todorov <veselin@veselin.bg>
2015-03-18 10:31:16 +0200

    Fix warnings

--- src/common/product-pane/pane.react.js
+++ src/common/product-pane/pane.react.js
@@ -11,7 +11,7 @@ let BodyModifier = require('../body-modifier/body-modifier.react');
 
 let closeButton = (
   <svg width="12px" height="12px" viewBox="0 0 12 12" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fill-rule="evenodd">
       <path d="M5,5 L5,-0.99912834 C5,-1.55536914 5.4477153,-2 6,-2 C6.5561352,-2 7,-1.5518945 7,-0.99912834 L7,5 L12.9991283,5 C13.5553691,5 14,5.4477153 14,6 C14,6.5561352 13.5518945,7 12.9991283,7 L7,7 L7,12.9991283 C7,13.5553691 6.5522847,14 6,14 C5.4438648,14 5,13.5518945 5,12.9991283 L5,7 L-0.99912834,7 C-1.55536914,7 -2,6.5522847 -2,6 C-2,5.4438648 -1.5518945,5 -0.99912834,5 L5,5 L5,5 Z" id="Rectangle-402" fill="#534540" transform="translate(6.000000, 6.000000) rotate(45.000000) translate(-6.000000, -6.000000) "></path>
     </g>
   </svg>
