9b1b188c4eb6afecc85e7aab4e229f0c982e9bab
Veselin Todorov <veselin@veselin.bg>
2015-04-01 11:37:51 +0300

    Load Typekit fonts on the options page

--- src/apps/options/main.js
+++ src/apps/options/main.js
@@ -4,12 +4,16 @@
 
 let React = require('react');
 let renderComponent = require('../../common/render');
+let loadTypekit = require('../../common/typekit');
 
 /**
  * Dependencies.
  */
 
 let Options = require('./components/options.react');
+
+// load Typekit
+loadTypekit();
 
 // render the component
 renderComponent(<Options />);
