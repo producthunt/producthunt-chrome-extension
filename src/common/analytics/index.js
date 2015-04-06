2d4dd0a8dd3abfb4b05bd16f602f545d68560c2b
Veselin Todorov <veselin@veselin.bg>
2015-04-06 16:02:41 +0300

    Refactor the analytics module

--- src/common/analytics/index.js
+++ src/common/analytics/index.js
@@ -2,8 +2,8 @@
  * Dependencies.
  */
 
-let debug = require('debug')('ph:analytics');
 let NullAnalytics = require('./null-analytics');
+let Tracker = require('./tracker');
 
 /**
  * Constants.
@@ -19,90 +19,8 @@ let enableAnalytics = window.ProductHuntAnalytics && ANALYTICS_KEY;
 let ProductHuntAnalytics = enableAnalytics ? window.ProductHuntAnalytics : NullAnalytics;
 let analytics = new ProductHuntAnalytics(process.env.ANALYTICS_KEY);
 
-module.exports = {
-
-  /**
-   * Track post click.
-   *
-   * @param {Object} post
-   */
-
-  clickPost(post) {
-    debug('track click post');
-    getAnonymousId(function(id) {
-      analytics.track({
-        anonymousId: id,
-        event: 'click',
-        properties: {
-          type: 'post',
-          link_location: 'index',
-          platform: 'chrome extension',
-          post_id: post.id,
-          post_name: post.name
-        }
-      });
-    });
-  },
-
-  /**
-   * Track bar click.
-   *
-   * @param {Object} post
-   * @public
-   */
-
-  clickBar(post) {
-    debug('track click bar');
-    getAnonymousId(function(id) {
-      analytics.track({
-        anonymousId: id,
-        event: 'click',
-        properties: {
-          type: 'post',
-          link_location: 'top_bar',
-          platform: 'chrome extension',
-          post_id: post.id,
-          post_name: post.name
-        }
-      });
-    });
-  }
-};
-
 /**
- * Generate random user id.
- *
- * @returns {String}
- * @private
+ * Export a new `Tracker`.
  */
 
-function anonymousId() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  });
-}
-
-/**
- * Get anonymous ID.
- *
- * @param {Function} fn
- * @private
- */
-
-function getAnonymousId(fn) {
-  chrome.storage.sync.get({ userId: null }, function(items) {
-    if (items.userId) {
-      debug('User ID found in cache');
-      return fn(items.userId);
-    }
-
-    debug('User ID not found in cache, generating a new one');
-
-    let userId = anonymousId();
-
-    chrome.storage.sync.set({ userId: userId }, function() {
-      fn(userId);
-    });
-  });
-}
+module.exports = new Tracker(analytics);
---
+++ src/common/analytics/tracker.js
@@ -0,0 +1,115 @@
+/**
+ * Dependencies.
+ */
+
+let debug = require('debug')('ph:analytics:tracker');
+
+/**
+ * Tracker.
+ *
+ * Analytics wrapper.
+ */
+
+class Tracker {
+
+  /**
+   * Constructor.
+   */
+
+  constructor(analytics) {
+    this.analytics = analytics;
+    this.platform = 'chrome extension';
+  }
+
+  /**
+   * Track post click.
+   *
+   * @param {Object} post
+   */
+
+  clickPost(post) {
+    this._trackPostClick(post, 'index');
+  }
+
+  /**
+   * Track bar click.
+   *
+   * @param {Object} post
+   * @public
+   */
+
+  clickBar(post) {
+    this._trackPostClick(post, 'top_bar');
+  }
+
+  /**
+   * Track post click on `location`.
+   *
+   * @param {Object} post
+   * @param {String} location
+   * @private
+   */
+
+  _trackPostClick(post, location) {
+    this._getAnonymousId((userId) => {
+      debug('track post click on "%s" for "%s"', location, userId);
+
+      this.analytics.track({
+        anonymousId: userId,
+        event: 'click',
+        properties: {
+          type: 'post',
+          link_location: location,
+          platform: this.platform,
+          post_id: post.id,
+          post_name: post.name
+        }
+      });
+    });
+  }
+
+  /**
+   * Generate random user id.
+   *
+   * @returns {String}
+   * @private
+   */
+
+  _anonymousId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }
+
+  /**
+   * Get anonymous ID for the current user, either from cache or
+   * generate and store a new one.
+   *
+   * @param {Function} callback
+   * @private
+   */
+
+  _getAnonymousId(cb) {
+    chrome.storage.sync.get({ userId: null }, (items) => {
+      if (items.userId) {
+        debug('User ID found in cache');
+        return cb(items.userId);
+      }
+
+      debug('User ID not found in cache, generating a new one');
+
+      let userId = this._anonymousId();
+
+      chrome.storage.sync.set({ userId: userId }, function() {
+        cb(userId);
+      });
+    });
+  }
+}
+
+/**
+ * Export `Tracker`.
+ */
+
+module.exports = Tracker;
