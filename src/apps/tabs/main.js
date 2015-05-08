223e876b36c25b80c40a3dc88104b37ffb6ec8a7
Veselin Todorov <veselin@veselin.bg>
2015-05-08 18:49:05 +0300

    Support building an extension without a default tab

--- src/apps/tabs/main.js
+++ src/apps/tabs/main.js
@@ -5,7 +5,6 @@
 let React = require('react');
 let debug = require('debug')('ph:tab:main');
 let DefaultTab = require('./components/DefaultTab.react');
-let DisabledDefaultTab = require('./components/DisabledDefaultTab.react');
 let renderComponent = require('../../common/render');
 let loadGoogleAnalytics = require('../../common/google-analytics');
 let settings = require('../../common/settings');
@@ -14,17 +13,7 @@ let settings = require('../../common/settings');
  * Constants.
  */
 
-const TAB_DISABLED_KEY = process.env.TAB_DISABLED_KEY;
 const GA_ID = process.env.GA_ID;
 
-settings.get(TAB_DISABLED_KEY, function(disabled) {
-  let el = document.getElementById('main');
-
-  if (disabled) {
-    debug('settings: tab disabled');
-    renderComponent(<DisabledDefaultTab />, el);
-  } else {
-    loadGoogleAnalytics(GA_ID);
-    renderComponent(<DefaultTab />, el);
-  }
-});
+loadGoogleAnalytics(GA_ID);
+renderComponent(<DefaultTab />, document.getElementById('main'));
